4a45d9d6d8eddd98882f9324289d79914a76ac2d
Jerous <tlu37317@gmail.com>
2016-09-13 20:58:15 +0800

    do service binding

--- app/src/main/java/com/tellustek/jerous/myfirstapp/LearnSerivce.java
+++ app/src/main/java/com/tellustek/jerous/myfirstapp/LearnSerivce.java
@@ -1,11 +1,15 @@
 package com.tellustek.jerous.myfirstapp;
 
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class LearnSerivce extends AppCompatActivity {
+public class LearnSerivce extends AppCompatActivity implements View.OnClickListener, ServiceConnection {
 
     private Intent intent;
 
@@ -16,7 +20,8 @@ public class LearnSerivce extends AppCompatActivity {
 
         intent = new Intent(LearnSerivce.this, MyService.class);
 
-        findViewById(R.id.btnStartService).setOnClickListener(new View.OnClickListener() {
+        //use this to refactor new View.OnClickListener()
+        /*findViewById(R.id.btnStartService).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //startService(new Intent(LearnSerivce.this, MyService.class));
@@ -32,6 +37,42 @@ public class LearnSerivce extends AppCompatActivity {
 
                 stopService(intent);
             }
-        });
+        });*/
+        findViewById(R.id.btnStartService).setOnClickListener(this);
+        findViewById(R.id.btnStopService).setOnClickListener(this);
+
+
+        findViewById(R.id.btnStartBindService).setOnClickListener(this);
+        findViewById(R.id.btnStopBindService).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnStartService:
+                startService(intent);
+                break;
+            case R.id.btnStopService:
+                stopService(intent);
+                break;
+            case R.id.btnStartBindService:
+                bindService(intent, this, Context.BIND_AUTO_CREATE);
+                break;
+            case R.id.btnStopBindService:
+                unbindService(this);
+                break;
+        }
+    }
+
+    //when service bind then do
+    @Override
+    public void onServiceConnected(ComponentName name, IBinder service) {
+        System.out.println("Service Connected.");
+    }
+
+    //when serivce unbind then do
+    @Override
+    public void onServiceDisconnected(ComponentName name) {
+
     }
 }
--- app/src/main/java/com/tellustek/jerous/myfirstapp/MyService.java
+++ app/src/main/java/com/tellustek/jerous/myfirstapp/MyService.java
@@ -2,6 +2,7 @@ package com.tellustek.jerous.myfirstapp;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 
 public class MyService extends Service {
@@ -11,9 +12,12 @@ public class MyService extends Service {
     @Override
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        //throw new UnsupportedOperationException("Not yet implemented");
+
+        return new Binder();
     }
 
+    //when start service will run onStartCommand
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
 
