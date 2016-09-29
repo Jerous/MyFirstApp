88c7650721401ac787ffa55a640bd1f277bd2c94
Jerous <tlu37317@gmail.com>
2016-09-29 21:54:01 +0800

    show bind serivce message on activity textview

--- app/src/main/java/com/tellustek/jerous/myfirstapp/LearnSerivce.java
+++ app/src/main/java/com/tellustek/jerous/myfirstapp/LearnSerivce.java
@@ -4,16 +4,20 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class LearnSerivce extends AppCompatActivity implements View.OnClickListener, ServiceConnection {
 
     private Intent intent;
     private EditText ServiceEditText;
+    private TextView ServiceOutText;
 
     private MyService.Binder binder = null;
 
@@ -24,6 +28,7 @@ public class LearnSerivce extends AppCompatActivity implements View.OnClickListe
 
         intent = new Intent(LearnSerivce.this, MyService.class);
         ServiceEditText = (EditText) findViewById(R.id.ServiceEditText);
+        ServiceOutText = (TextView) findViewById(R.id.ServiceOutText);
 
         //use this to refactor new View.OnClickListener()
         /*findViewById(R.id.btnStartService).setOnClickListener(new View.OnClickListener() {
@@ -83,6 +88,23 @@ public class LearnSerivce extends AppCompatActivity implements View.OnClickListe
         System.out.println("Service Connected.");
 
         binder = (MyService.Binder) service;
+
+        //以下就可以訪問到myservice內部的數據，再activity中讀取
+        binder.getService().setCallback(new MyService.Callback() {
+            @Override
+            public void onDataChange(String data) {
+                //不能這樣寫，android的安全機制不允許直接修改Serivce中的new Thread
+                //ServiceOutText.setText(data);
+                //要另外用handler去call
+
+                Message msg = new Message();  //建message存要傳遞的訊息
+                Bundle b = new Bundle();  //建bundle存data
+                b.putString("data", data);  //將data存到bundle中
+                msg.setData(b);  //Message的setdata方法
+                handler.sendMessage(msg);  //將msg傳到handler中
+
+            }
+        });
     }
 
     //when serivce was break or killed then do
@@ -90,4 +112,14 @@ public class LearnSerivce extends AppCompatActivity implements View.OnClickListe
     public void onServiceDisconnected(ComponentName name) {
 
     }
+
+    private Handler handler = new Handler() {
+
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+
+            ServiceOutText.setText(msg.getData().getString("data"));
+        }
+    };
 }
--- app/src/main/java/com/tellustek/jerous/myfirstapp/MyService.java
+++ app/src/main/java/com/tellustek/jerous/myfirstapp/MyService.java
@@ -9,6 +9,7 @@ public class MyService extends Service {
 
     private boolean serviceRunning = false;
     private String data = "This is bind message...";
+    private int i;
 
     public MyService() {
     }
@@ -16,8 +17,14 @@ public class MyService extends Service {
     //use textedit to change bind service message.
     public class Binder extends android.os.Binder {
 
+        //讓service外可以訪問到service內的data變數
         public void setData(String data) {
             MyService.this.data = data;
+        }
+
+        //讓service外可以訪問到service內的東西
+        public MyService getService() {
+            return MyService.this;
         }
     }
 
@@ -35,7 +42,6 @@ public class MyService extends Service {
 
         System.out.println("on Start Command.");
         data = intent.getStringExtra("data");
-
         return super.onStartCommand(intent, flags, startId);
     }
 
@@ -53,8 +59,20 @@ public class MyService extends Service {
             public void run() {
                 super.run();
 
+                i = 0;
+
                 while (serviceRunning) {
-                    System.out.println(data);
+
+                    i++;
+
+                    String str = i+" : "+data;
+                    System.out.println(str);
+
+
+                    //傳出去service外面
+                    if (callback != null) {
+                        callback.onDataChange(str);
+                    }
 
                     try {
                         sleep(2000);
@@ -73,5 +91,24 @@ public class MyService extends Service {
         System.out.println("Service Destroy.");
 
         serviceRunning = false;
+    }
+
+
+
+    //定義一個跟service外連接的介面function
+    public static interface Callback{
+        void onDataChange(String data);
+    }
+
+    //實例化callback
+    private Callback callback = null;
+
+    //定義callback介面的set與getCallback方法
+    public void setCallback(Callback callback) {
+        this.callback = callback;
+    }
+
+    public Callback getCallback() {
+        return callback;
     }
 }
