4a52162cc36b741b0d705433c16723cf431db705
Jerous <tlu37317@gmail.com>
2016-10-19 21:10:04 +0800

    Learn Component radio and checkbox

--- app1/src/main/java/com/tellustek/jerous/app1/LearnComponent.java
+++ app1/src/main/java/com/tellustek/jerous/app1/LearnComponent.java
@@ -8,33 +8,53 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.DatePicker;
+import android.widget.RadioButton;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
-public class LearnComponent extends AppCompatActivity {
+public class LearnComponent extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
 
-    private Spinner s;
+    private Spinner spinner1;
     private String[] dataSource = new String[]{"Jerous", "Tellustek", "tlu3"};
 
-    private Button btnChooseDate;
-    private Button btnChooseTime;
+    private Button btnChooseDate, btnChooseTime, btnRadioSubmit;
+
+    private RadioButton RadioButton1;
+
+    private CheckBox checkBox1, checkBox2, checkBox3;
+    private TextView tVcBout;
+
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_learn_component);
 
-            s = (Spinner) findViewById(R.id.spinner);
+            spinner1 = (Spinner) findViewById(R.id.spinner);
+
             btnChooseDate = (Button) findViewById(R.id.btnChooseDate);
+
             btnChooseTime = (Button) findViewById(R.id.btnChooseTime);
+
+            RadioButton1 = (RadioButton) findViewById(R.id.radioButton1);
+            btnRadioSubmit = (Button) findViewById(R.id.btnRadioSubmit);
+
+            checkBox1 = (CheckBox) findViewById(R.id.checkBox1);
+            checkBox2 = (CheckBox) findViewById(R.id.checkBox2);
+            checkBox3 = (CheckBox) findViewById(R.id.checkBox3);
+            tVcBout = (TextView) findViewById(R.id.tVcBout);
 
 
             //提供一個spinner數據
-            s.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, dataSource));
+            spinner1.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, dataSource));
 
             //監聽spinner選擇事件
-            s.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
                 @Override
                 public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                     System.out.println("用戶選擇的是"+dataSource[position]);
@@ -80,5 +100,36 @@ public class LearnComponent extends AppCompatActivity {
                     },0,0,true).show();  //初始化時間
                 }
             });
+
+            btnRadioSubmit.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (RadioButton1.isChecked()) {
+                        Toast.makeText(LearnComponent.this, "所選的是正確的", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(LearnComponent.this, "所選的是錯誤的", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
+            checkBox1.setOnCheckedChangeListener(this);
+            checkBox2.setOnCheckedChangeListener(this);
+            checkBox3.setOnCheckedChangeListener(this);
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        String str = "你喜歡 ";
+
+        if (checkBox1.isChecked()){
+            str += checkBox1.getText() + ", ";
+        }
+        if (checkBox2.isChecked()){
+            str += checkBox2.getText() + ", ";
+        }
+        if (checkBox3.isChecked()){
+            str += checkBox3.getText();
+        }
+        tVcBout.setText(str);
     }
 }
